d59588d9749aa6fdbebb544e081134e10d77634f
bradhead <brad.head@gmail.com>
2023-12-07 16:04:45 -0800

    created build

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # kc-attribute-mapper-spi
 A Keycloak identity provider SPI to fix an issue with attribute mappers not firing during Token Exchange
+
+## Generate POM
+
+```shell
+mvn archetype:generate -DgroupId=org.bradhead.keycloak -DartifactId=keycloak-extension -DarchetypeArtifactId=maven-archetype-quickstart -D-DinteractiveMode=false
+```
+
+## Build the extension
+
+```shell
+cd keycloak-extension && mvn clean install && cd .
+```
+
+## Deploy to Keycloak
+
+```shell
+cp keycloak-extension/target/keycloak-extension-bradhead-1.0-SNAPSHOT.jar /opt/keycloak/providers/.
+kc.sh build
---
+++ keycloak-extension/src/main/java/org/bradhead/keycloak/UserAttributeMapper.java
@@ -0,0 +1,32 @@
+/**
+ * When creating user during token exchange federation, attributes should be updated (created in this case) as well
+ */
+package org.bradhead.keycloak;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.IdentityProviderMapperModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.RealmModel;
+import org.keycloak.broker.provider.BrokeredIdentityContext;
+
+/**
+ * Maps a UserModel.attribute to a token claim and/or token response field.
+ * The token claim name can be a full qualified nested object name, e.g. "address.country".
+ * This will create a nested json object within the token claim.
+ * 
+ * Fixes issue when attribute mapper not working when creating user during token exchange federation
+ *
+ * @author <a href="mailto:brad@zedwerks.com">Brad Head</a>
+ * @version $Revision: 1 $
+ */
+public class UserAttributeMapper extends org.keycloak.broker.oidc.mappers.UserAttributeMapper{
+    @Override
+    public void importNewUser(KeycloakSession session, RealmModel realm, UserModel user, IdentityProviderMapperModel mapperModel, BrokeredIdentityContext context) {
+        super.importNewUser(session, realm, user, mapperModel, context);
+        this.updateBrokeredUser(session, realm, user, mapperModel, context);
+    }
+}
